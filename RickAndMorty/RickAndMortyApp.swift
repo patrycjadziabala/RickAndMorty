55e740e34857f5fc599bb9dc6980a396a5ea054c
Patrycja Dziabala <patrycja.dziabala@gmail.com>
2023-09-07 22:54:49 +0200

    Initial Commit

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,20 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Patka on 07/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
